2cd2cf91bccd2ed9c18814ec654e7edb204e25ef
alex <alex@shvarev.com>
2024-04-23 20:45:03 +0300

    v1.0.2 HotFix

--- OddsServer/settings.py
+++ OddsServer/settings.py
@@ -165,7 +165,11 @@ API_ADDRESS = os.getenv("API_ADDRESS")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- web.sh
+++ web.sh
@@ -2,4 +2,4 @@
 
 python3 manage.py collectstatic --clear --skip-checks --noinput
 python3 manage.py migrate
-gunicorn BondStats.wsgi:application -w 1 -t 600 -b 0.0.0.0:220
+gunicorn OddsServer.wsgi:application -w 1 -t 600 -b 0.0.0.0:220
